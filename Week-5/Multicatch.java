0078af3fff01b64769e2e4e2ae683af6d9a621bf
pavan999512 <pavanbonagiri999@gmail.com>
2026-03-11 12:12:08 +0530

    Add files via upload

---
+++ Week-5/Multicatch.java
@@ -0,0 +1,27 @@
+public class Multicatch
+{
+	public static void main(String[] args)
+	{
+		try{
+			int a = args.length;
+			System.out.println("a = " + a);
+			int b = 42/a;
+			System.out.println(b);
+			int[] c = {1};
+			c[42] = 18;
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Divide by 0 VJIT"  + e);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Array index out of bound : " + e);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Divide by 0:" + e);
+		}
+		System.out.println("After try catch blocks");
+	}
+}
---
+++ Week-5/Testbalance.java
@@ -0,0 +1,9 @@
+import Mypack.Balance;
+public class Testbalance
+{
+	public static void main(String[] args)
+	{
+		Balance test = new Balance("VJIT" , 100000);
+		test.show();
+	}
+}
